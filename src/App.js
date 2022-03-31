b4d1ba031e944d56c9b428253ce3b2cbfe77c67a
laurent-aubry <46169816+laurent-aubry@users.noreply.github.com>
2022-03-31 14:28:55 +0200

    exercice terminé

--- src/App.js
+++ src/App.js
@@ -1,22 +1,46 @@
 // import logo from './logo.svg';
 // import './App.css';
+import { useState } from 'react';
+import isEmail from 'validator/lib/isEmail';
 
 function App() {
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const handleValidation = (e) => {
+    e.preventDefault();
+    setError("");
+    !isEmail(email) ? setError("Erreur de saisie sur l'email") : setError("");
+    if(password.length<5){
+      setError("Le mdp doit avoir au minimum 4 caractères")
+    }
+  }
+
   return (
     <div className='container my-5' >
-      <form>
+      <form onSubmit={handleValidation}>
         <div className='mb-3'>
           <label htmlFor="email" className='form-label' >Email</label>
-          <input id="email" name="email" type="email" className='form-control' />
+          <input id="email" name="email" type="email" className='form-control'
+          value={email}
+          onChange={event => setEmail(event.target.value)}
+          />
         </div>
         <div className='mb-3'>
           <label htmlFor="password" className='form-label' >Password</label>
-          <input id="password" name="password" type="password" className='form-control' />
+          <input id="password" name="password" type="password" className='form-control'
+                    value={password}
+                    onChange={event => setPassword(event.target.value)}
+          />
         </div>
         <div className='mb-3'>
           <label htmlFor="confirm-password" className='form-label' >Confirm Password</label>
           <input id="confirm-password" name="confirm-password" type="password" className='form-control' />
         </div>
+        {error && <p className='alert'>{error}</p>}
+        <button type="submit" >Valider</button>
       </form>
     </div>
   );
--- src/App.test.js
+++ src/App.test.js
@@ -1,16 +1,86 @@
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event'
 import App from './App';
 
-test('renders learn react link', () => {
+beforeEach(() => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-  const linkElement2 = screen.queryByText(/learn testing/i);
-  expect(linkElement2).not.toBeInTheDocument();
 });
 
-test('renders learn testing link', () => {
-  render(<App />);
-  const linkElement2 = screen.queryByText(/learn testing/i);
-  expect(linkElement2).not.toBeInTheDocument();
-});
+beforeAll(() => { });
+
+afterEach(() => { });
+
+afterAll(() => { });
+
+const typeIntoForm = ({ email, password, confirmpassword }) => {
+  const emailInputElement = screen.getByRole("textbox", { name: /email/i })
+  const pwdInputElement = screen.getByLabelText("Password");
+  const confirmPwdInputElement = screen.getByLabelText(/confirm password/i);
+
+  if (email) {
+    userEvent.type(emailInputElement, email);
+  }
+  if (password) {
+    userEvent.type(pwdInputElement, password);
+  }
+  if (confirmpassword) {
+    userEvent.type(confirmPwdInputElement, confirmpassword);
+  }
+
+  return {
+    emailInputElement,
+    pwdInputElement,
+    confirmPwdInputElement
+  }
+};
+
+const clickOnSubmitButton = () => {
+  const submitBtnElement = screen.getByRole("button", {
+    name: /valider/i
+  });
+  userEvent.click(submitBtnElement);
+}
+
+describe("Contrôle sur composant App", () => {
+
+  test('renders login form', () => {
+    expect(screen.getByRole("textbox", { name: /email/i }).value).toBe("");
+    expect(screen.getByLabelText("Password").value).toBe("");
+    expect(screen.getByLabelText(/confirm password/i).value).toBe("");
+  });
+
+  test('Doit avoir un email', () => {
+    const { emailInputElement } = typeIntoForm({ email: "test@gmail.com" })
+    expect(emailInputElement.value).toBe("test@gmail.com");
+  })
+
+  test('Peut saisir un mdp', () => {
+    const { pwdInputElement } = typeIntoForm({ password: "test123!" })
+    expect(pwdInputElement.value).toBe("test123!");
+  })
+
+  describe("Erreur de saisie", () => {
+
+
+    test("Erreur sur email invalide", () => {
+      expect(screen.queryByText(/erreur de saisie sur l'email/i)).not.toBeInTheDocument();
+
+      typeIntoForm({ password: "12345678" })
+
+      typeIntoForm({ email: "test.gmail.com" })
+      clickOnSubmitButton();
+
+      expect(screen.queryByText(/erreur de saisie sur l'email/i)).toBeInTheDocument()
+    })
+
+    test("Le mdp doit avoir au moins 5 caractères", () => {
+      expect(screen.queryByText(/le mdp doit avoir au minimum 4 caractères/i)).not.toBeInTheDocument();
+
+      typeIntoForm({ password: "123" })
+      clickOnSubmitButton();
+
+      expect(screen.queryByText(/le mdp doit avoir au minimum 4 caractères/i)).toBeInTheDocument();
+    })
+
+  })
+})
